4906df0ada55a1fe3bbdd7132bc8eb4a9e868e4f
nguy0890 <nguy0890@mylaurier.ca>
2022-11-26 00:35:40 -0500

    Initial commit

---
+++ app/src/main/java/com/example/healthbot/SplashScreen.java
@@ -0,0 +1,59 @@
+package com.example.healthbot;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+public class SplashScreen extends AppCompatActivity {
+
+
+    ImageView splashLogo;
+    ImageView redBar;
+    ImageView blueBar;
+    TextView splashName;
+    TextView splashSlogan;
+    Animation topAnimation;
+    Animation botAnimation;
+    Handler handler;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+
+        // Hide action bar
+        if (getSupportActionBar() != null) {
+            getSupportActionBar().hide();
+        }
+        //Animation for logo
+        topAnimation = AnimationUtils.loadAnimation(this, R.anim.from_top);
+        botAnimation = AnimationUtils.loadAnimation(this, R.anim.from_bottom);
+
+        redBar = findViewById(R.id.redBar);
+        blueBar = findViewById(R.id.blueBar);
+        splashLogo = findViewById(R.id.splashLogo);
+        splashName = findViewById(R.id.tvappName);
+        splashSlogan = findViewById(R.id.tvappSlogan);
+
+        redBar.setAnimation(topAnimation);
+        blueBar.setAnimation(botAnimation);
+        splashLogo.setAnimation(topAnimation);
+        splashName.setAnimation(botAnimation);
+        splashSlogan.setAnimation(botAnimation);
+
+        //Delay for 3 seconds
+        handler = new Handler();
+        handler.postDelayed(new Runnable () {
+            public void run() {
+                startActivity(new Intent(SplashScreen.this, LoginActivity.class));
+                finish();
+            }
+        }, 3000);
+    }
+}
