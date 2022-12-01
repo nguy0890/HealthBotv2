974b19f951e40f6052d616e1e3b5b984cf2b1a43
Andrew <best0060@mylaurier.ca>
2022-12-01 17:17:59 -0500

    initial commit - migrated and added details fragment to diagnosis history

--- app/src/main/java/com/example/healthbot/MainActivity.java
+++ app/src/main/java/com/example/healthbot/MainActivity.java
@@ -2,7 +2,14 @@ package com.example.healthbot;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import java.util.Calendar;
+import java.util.Date;
 
 
 public class MainActivity extends AppCompatActivity {
@@ -13,6 +20,33 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        Button chatBtn = findViewById(R.id.chatBtn);
+        Button histBtn = findViewById(R.id.histBtn);
 
+        chatBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                addHistory();
+            }
+        });
+
+        histBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, HistoryActivity.class);
+                startActivityForResult(intent, 10);
+            }
+        });
+
+
+    }
+    //temporary
+    protected void addHistory(){
+        SharedPreferences dh_sp = getSharedPreferences("history_sp", MODE_PRIVATE);
+        Date currentTime = Calendar.getInstance().getTime();
+        SharedPreferences.Editor dh_sp_edit = dh_sp.edit();
+
+        dh_sp_edit.putString(currentTime.toString(), "Diagnosis Description");
+        dh_sp_edit.commit();
     }
 }
