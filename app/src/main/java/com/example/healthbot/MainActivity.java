51aa3c0c668d9f2f89dabc825db92dfee23f9414
nguy0890 <nguy0890@mylaurier.ca>
2022-12-01 17:22:36 -0500

    Added BMI calculator
    BMI calculator activity, layout, binding in gradle

--- app/src/main/java/com/example/healthbot/MainActivity.java
+++ app/src/main/java/com/example/healthbot/MainActivity.java
@@ -2,10 +2,15 @@ package com.example.healthbot;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
 
 public class MainActivity extends AppCompatActivity {
+    protected static final String ACTIVITY_NAME = "MainActivity"; //debugging message
 
 
     @Override
@@ -13,6 +18,18 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        //Declare button
+        final Button bmiButton = findViewById(R.id.bmiButton);
 
+        //Toolbar button on click
+        bmiButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Log.i(ACTIVITY_NAME, "User clicked BMI Button");
+                Intent intent = new Intent(MainActivity.this, BMICalculator.class);
+                startActivityForResult(intent, 10);
+            }
+        });
     }
+
 }
