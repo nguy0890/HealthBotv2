f1d3f9de8883755e3c413d21fce90c75ea8152df
ArvindSahota1024 <arvind.s@live.com>
2022-12-02 21:29:28 -0500

    added bmi chart and snackbar

--- app/src/main/java/com/example/healthbot/BMICalculator.java
+++ app/src/main/java/com/example/healthbot/BMICalculator.java
@@ -2,18 +2,26 @@ package com.example.healthbot;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Dialog;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.view.Window;
 import android.widget.Button;
 import android.widget.NumberPicker;
 import android.widget.Spinner;
 
 import com.example.healthbot.databinding.ActivityBmicalculatorBinding;
 import com.example.healthbot.databinding.ActivityMainBinding;
+import com.google.android.material.snackbar.Snackbar;
 
 public class BMICalculator extends AppCompatActivity {
+    protected static final String ACTIVITY_NAME = "BMICalculator";
 
     private ActivityBmicalculatorBinding binding;
+    Dialog chart_dia;
 
 
     @Override
@@ -23,12 +31,22 @@ public class BMICalculator extends AppCompatActivity {
         View view = binding.getRoot();
         setContentView(view);
 
+        Button openChart = findViewById((R.id.OpenChart));
         Button back_arrow = findViewById(R.id.bmi_back_arrow);
 
         back_arrow.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 finish();
+            }
+        });
+
+        chart_dia = new Dialog(this); //open bmi chart
+        openChart.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Log.i(ACTIVITY_NAME, "User clicked Show Chart Button");
+                openChartDialog();
             }
         });
 
@@ -76,6 +94,23 @@ public class BMICalculator extends AppCompatActivity {
         }
         binding.BMIResultText.setText(bmiResult);
 
+    }
+
+    private void openChartDialog() {
+        chart_dia.setContentView(R.layout.bmi_chart);
+        chart_dia.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+        Button chartClose = chart_dia.findViewById(R.id.CloseChart);
+        chart_dia.show();
+
+        chartClose.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Log.i(ACTIVITY_NAME, "User clicked Close Chart Button");
+                chart_dia.dismiss();
+                Snackbar snack = Snackbar.make(findViewById(R.id.bmilayout), getString(R.string.CloseSnack), Snackbar.LENGTH_LONG);
+                snack.show();
+            }
+        });
     }
 
 }
